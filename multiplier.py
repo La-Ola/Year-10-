7be86567f7fc46262b8b4c3a3b194a667ab5c6d4
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2019-01-12 21:29:53 +0000

    Update multiplier.py

--- multiplier.py
+++ multiplier.py
@@ -1,17 +1,17 @@
 
 #MULTIPLIER
 
-play = str(input("Would you like a multiplier program? (Please type 'Yes'.)"))
+play = str(input("Would you like a multiplier program? (Please type 'Yes'.)")) #makes sure that the user input is a string
 
-while play== "Yes":
-    sn = int(input("What number would you like you start point to be?"))
-    en = int(input("How many times would you like it to repeat?"))
-    m = int(input("What would you like your multiplier to be?"))
+while play == "Yes":
+    StartNumber = int(input("What number would you like you start point to be?"))
+    EndNumber = int(input("How many times would you like it to repeat?"))
+    multiplier = int(input("What would you like your multiplier to be?"))
 
-    print ("**********",m,"TIMES TABLE **********")
-    for i in range (sn,en):
-        ans = i * m
-        print ("",i,"*",m,"=",ans)
+    print ("**********",multiplier,"TIMES TABLE **********")
+    for index in range (StartNumber, EndNumber):
+        ans = index * multiplier
+        print (index,"*",multiplier,"=",ans)
 
-    play = str(input("Would you like to reuse the program? Please enter Yes or No."))
+    play = str(input("Would you like to reuse the program? Please enter Yes or No.")) #continues or exits the loop
 print("Goodbye.")

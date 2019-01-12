fda529d987ae0ba2c20dcdd3d8edf2cc9b88e1d6
Stephanie Skinner <44809906+La-Ola@users.noreply.github.com>
2019-01-12 21:22:47 +0000

    Update number game.py

--- number game.py
+++ number game.py
@@ -1,12 +1,12 @@
 
 #Guessing Gameimport random
-import random
-number= random.randint (1,50)
-ans = int(input("Choose a number between 1 - 50."))
-while ans != number:
-    if ans<number:
+import random #imports the random dictionary
+number= random.randint (1,50) #chooses a random integer between given number and asigns it to the variable
+ans = int(input("Choose a number between 1 - 50.")) #turns the string input into an integer
+while ans != number: #wont break loop until answer is the same as the number chosen at random
+    if ans<number: #answer is less than number 
         print ("you are too low")
-    elif ans>number:
+    elif ans>number: #answer is more than number 
         print ("you are too high")
     ans = int(input("guess again"))
 print("well done!")
